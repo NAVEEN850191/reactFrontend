599e959773e18fdab4810cc24673fbfa8ed61670
NAVEEN850191 <gopidesinaveen327@gmail.com>
2026-07-06 12:56:12 +0530

    challenge 2

---
+++ courses/02-redux-rtk-query/project/src/store/slices/counterSlice.ts
@@ -0,0 +1,27 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+interface CounterState{
+    count:number;
+}
+
+const initialState:CounterState={
+    count:0,
+};
+
+export  const counterSlice = createSlice({
+    name:'counter',
+    initialState,
+    reducers:{
+        increment:(state) =>{
+            state.count+=1;
+        },
+
+        decrement:(state)=>{
+            state.count-=1;
+        },
+    },
+
+});
+
+export default counterSlice.reducer;
+export const {increment,decrement} = counterSlice.actions;
