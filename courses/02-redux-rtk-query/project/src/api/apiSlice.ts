fd8f51e489f700329f80ae45aba4e4cd21e6889e
NAVEEN850191 <gopidesinaveen327@gmail.com>
2026-07-11 13:46:05 +0530

    challenege 7

--- courses/02-redux-rtk-query/project/src/api/apiSlice.ts
+++ courses/02-redux-rtk-query/project/src/api/apiSlice.ts
@@ -1,11 +1,18 @@
 import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react'
 import { mockApi } from './mockServer'
 
+interface User {
+  id: number
+  name: string
+  email: string
+  username: string
+}
+
 export const apiSlice = createApi({
   reducerPath: 'api', 
   baseQuery: fakeBaseQuery(),
   endpoints: (builder) => ({
-    getUsers: builder.query({
+    getUsers: builder.query<User[], void>({
       queryFn: async () => {
         const users = await mockApi.getUsers()
         return { data: users }
--- courses/02-redux-rtk-query/project/src/components/UsersList.tsx
+++ courses/02-redux-rtk-query/project/src/components/UsersList.tsx
@@ -1,4 +1,38 @@
 /** Stub: Complete Challenge 07 (Queries) per README. */
+import {useGetUsersQuery} from '../api/apiSlice'
+//imported  useQueryHook fromapiSlice
+
 export default function UsersList() {
-  return <div id="users-list">Complete Challenge 07 per README.</div>
+
+  const {data,isLoading,isError,error}=useGetUsersQuery()
+  if (isLoading) {
+    return (
+      <div data-testid="users-loading">
+        Loading...
+      </div>
+    )
+  }
+
+  if (isError || error) {
+    return (
+      <div data-testid="users-error">
+        Error loading users
+      </div>
+    )
+  }
+
+
+
+  return (
+  
+    <div data-testid="users-list">
+      {data?.map((user) => (
+        <div key={user.id}>
+          <h3>{user.name}</h3>
+          <p>{user.email}</p>
+          <p>{user.username}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
