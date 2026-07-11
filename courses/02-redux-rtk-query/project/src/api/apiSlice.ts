d45e31744a752d909af3e1e53f1746acc0cfaeae
NAVEEN850191 <gopidesinaveen327@gmail.com>
2026-07-11 15:20:25 +0530

    challenge 8

--- courses/02-redux-rtk-query/project/src/api/apiSlice.ts
+++ courses/02-redux-rtk-query/project/src/api/apiSlice.ts
@@ -6,19 +6,60 @@ interface User {
   name: string
   email: string
   username: string
+  phone:string
 }
 
 export const apiSlice = createApi({
   reducerPath: 'api', 
+
   baseQuery: fakeBaseQuery(),
+
+  tagTypes: ['User', 'Post'],
+
   endpoints: (builder) => ({
     getUsers: builder.query<User[], void>({
       queryFn: async () => {
         const users = await mockApi.getUsers()
         return { data: users }
       },
+
+      providesTags: (result) =>result ? [
+              ...result.map(({ id }) => ({
+                type: 'User' as const,
+                id,
+              })),
+              {
+                type: 'User',
+                id: 'LIST',
+              },
+            ]
+          : [
+              {
+                type: 'User',
+                id: 'LIST',
+              },
+            ],
     }),
-  }),
-})
+
+
+    addUser: builder.mutation<User, Omit<User,'id'>>({
+      queryFn: async (user) => {
+        const newUser = await mockApi.createUser(user)
+
+        return {
+          data: newUser,
+        }
+      },
+
+      invalidatesTags: [
+        {
+          type: 'User',
+          id: 'LIST',
+        },
+      ],
+    }),
+
+    }),
+  })
 
 export const { useGetUsersQuery } = apiSlice
