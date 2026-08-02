66588b83a6fc5542201364af36414a1e9227c003
NAVEEN850191 <gopidesinaveen327@gmail.com>
2026-08-02 15:38:27 +0530

    challenge 13

--- courses/03-nextjs-app-router/project/app/layout.tsx
+++ courses/03-nextjs-app-router/project/app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from 'next'
 import Link from "next/link";
 import './globals.css'
+import { Inter } from 'next/font/google'
 //serverComponent, fileBasedRouting, appDirectory
 // useState,useClient
 // generateMetadata
+// nextImage, nextFont
+const inter = Inter({
+  subsets: ['latin'],
+})
+
 export const metadata: Metadata = {
   title: 'Next.js App Router Project',
   description: 'Complete challenges to build your Next.js skills',
@@ -16,12 +22,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-       <nav>
+      <body className={inter.className}>
+        <nav>
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
         </nav>
-      {children}</body>
+
+        {children}
+      </body>
     </html>
   )
 }
--- courses/03-nextjs-app-router/project/app/page.tsx
+++ courses/03-nextjs-app-router/project/app/page.tsx
@@ -1,16 +1,24 @@
 import Link from "next/link";
 export const dynamic = "force-static";
 import Counter from "./components/Counter";
+import Image from 'next/image'
 //serverComponent, fileBasedRouting, appDirectory
 // useState,useClient
 //dynamicExport, forceStaticOrDynamic
 // metadata, generateMetadata
+// nextImage, nextFont
 export default function HomePage() {
   return (
     <main>
       <h1>Home</h1>
       <Link href="/about">About</Link>
       <Counter />
+      <Image
+        src="https://placehold.co/300x200"
+        alt="Placeholder image"
+        width={300}
+        height={200}
+      />
     </main>
   );
 }
